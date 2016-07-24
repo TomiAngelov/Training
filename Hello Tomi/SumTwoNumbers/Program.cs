952189408c62945f785b9b2bc1bc1580de1726dd
astian <abrashev92@gmail.com>
2016-07-24 18:26:36 +0300

    Sample Project commit

---
+++ Hello Tomi/SumTwoNumbers/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SumTwoNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1 = int.Parse(Console.ReadLine());
+            int num2 = int.Parse(Console.ReadLine());
+
+            int result = num1 + num2;
+            Console.WriteLine(result);
+            Console.ReadLine();
+        }
+    }
+}
